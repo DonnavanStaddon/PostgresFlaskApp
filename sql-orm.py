f0c1ae7e23bdfae5dff2602a4bdff19fda6f611f
Donnavan Staddon <93292844+DonnavanStaddon@users.noreply.github.com>
2023-09-05 13:13:19 +0100

    Query classes from session

--- sql-orm.py
+++ sql-orm.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 # executing the instructions from our localhost "chinook" db
-db = create_engine("postgresql://postgres:@localhost:5432/chinook")
+db = create_engine("postgresql://postgres:litStunt01@localhost:5432/chinook")
 base = declarative_base()
 
 
@@ -32,7 +32,7 @@ class Track(base):
     Name = Column(String)
     AlbumId = Column(Integer, ForeignKey("Album.ArtistId"))
     MediaTypeId = Column(Integer, primary_key=False)
-    GenId = Column(Integer, primary_key=False)
+    GenreId = Column(Integer, primary_key=False)
     Composer = Column(String)
     Milliseconds = Column(Integer, primary_key=False)
     Bytes = Column(Integer, primary_key=False)
@@ -51,6 +51,45 @@ base.metadata.create_all(db)
 
 
 # Query 1 - select all records from the "Artist" table
-artists = session.query(Artist)
-for artist in artists:
-    print(artist.ArtistId, artist.Name, sep=" | ")
+# artists = session.query(Artist)
+# for artist in artists:
+    # print(artist.ArtistId, artist.Name, sep=" | ")
+
+
+# Query 2 - select only the "Name" from the "Artist" table
+# artists = session.query(Artist)
+# for artist in artists:
+    # print(artist.Name)
+
+
+# Query 3 - select only "Queen" from the "Artist" table
+# artist = session.query(Artist).filter_by(Name="Queen").first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+
+# Query 4 - select only "ArtistId" #51 from the "Artist" table
+# artist = session.query(Artist).filter_by(ArtistId=51).first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
+
+
+# Query 5 - select only the albums with "ArtistId" #51 from the "Album" table
+# albums = session.query(Album).filter_by(ArtistId=51)
+# for album in albums:
+    # print(album.ArtistId, album.Title, album.ArtistId, sep=" | ")
+
+
+# Query 6 - select all tracks where ther composer is "Queen" from the "Track" table
+tracks = session.query(Track).filter_by(Composer="Queen")
+for track in tracks:
+    print(
+        track.TrackId,
+        track.Name,
+        track.AlbumId,
+        track.MediaTypeId,
+        track.GenreId,
+        track.Composer,
+        track.Milliseconds,
+        track.Bytes,
+        track.UnitPrice,
+        sep=" | "
+    )
