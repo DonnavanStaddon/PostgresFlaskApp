70173435c90ee97c488c3433d2f9a601adb54985
Donnavan Staddon <93292844+DonnavanStaddon@users.noreply.github.com>
2023-09-06 20:25:35 +0100

    SQLAlchemy-Update And Delete

--- crud.py
+++ crud.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 # executing the instructions from our localhost "chinook" db
-db = create_engine("postgresql://postgres:PASSWORD@localhost:5432/chinook")
+db = create_engine("postgresql://postgres:litStunt01@localhost:5432/chinook")
 base = declarative_base()
 
 # crate table
@@ -98,10 +98,48 @@ donnavan_staddon = Programmer(
 # session.add(margaret_hamilton)
 # session.add(bill_gates)
 # session.add(tim_berners_lee)
-session.add(donnavan_staddon)
+# session.add(donnavan_staddon)
+
+# updating a single record
+# programmer = session.query(Programmer).filter_by(id=8).first()
+# programmer.famous_for = "World President"
 
 # insert data into bd
-session.commit()
+# session.commit()
+
+# updating multiple records
+# people = session.query(Programmer)
+#for person in people:
+    #if person.gender == "F":
+        #person.gender = "Female"
+    #elif person.gender == "M":
+        #person.gender = "Male"
+    #else:
+        #print("Gwnder not defined")
+
+
+# delete all records
+#programmers = session.query(Programmer)
+#for programmer in programmers:
+    #session.delete(programmer)
+    #session.commit()
+
+# deleting a single records
+#fname = input("Enter a first name: ")
+#lname = input("Enter a last name: ")
+#programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# defensive programming
+#if programmer is not None:
+    #print("Programmer Found: ", programmer.first_name + " " + programmer.last_name)
+    #confirmation = input("Are you sure you want to delete this record? (y/n) ")
+    #if confirmation.lower() == "y":
+        #session.delete(programmer)
+        #session.commit()
+        #print("Programmer has been deleted")
+    #else:
+        #print("Programmer not deleted")
+#else:
+    #print("No records found")
 
 # view/query data in Programmer Table
 programmers = session.query(Programmer)
